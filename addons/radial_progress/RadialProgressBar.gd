cc46f1f1ae38a71ee9a3fb284f4ee2e69445a963
daveTheOldCoder <55119684+daveTheOldCoder@users.noreply.github.com>
2023-09-26 17:58:33 -0400

    Cleanup.

--- addons/radial_progress/RadialProgressBar.gd
+++ addons/radial_progress/RadialProgressBar.gd
@@ -10,25 +10,25 @@ extends Control
 @export var bar_color := Color(0.2, 0.9, 0.2, 1.0)
 
 
-func set_max(value: float) -> void:
-	max_value = value
+func set_max(_max_value: float) -> void:
+	max_value = _max_value
 	_draw()
 
 
-func set_progress(v: float) -> void:
-	progress = v
+func set_progress(_progress: float) -> void:
+	progress = _progress
 	queue_redraw()
 
 
-func set_radius_and_thickness(r: float, th: float) -> void:
-	radius = r
-	thickness = th
+func set_radius_and_thickness(_radius: float, _thickness: float) -> void:
+	radius = _radius
+	thickness = _thickness
 	queue_redraw()
 
 
-func set_colors(bg: Color, bar: Color) -> void:
-	bg_color = bg
-	bar_color = bar
+func set_colors(_bg_color: Color, _bar_color: Color) -> void:
+	bg_color = _bg_color
+	bar_color = _bar_color
 	queue_redraw()
 
 
@@ -39,15 +39,15 @@ func _draw() -> void:
 	draw_circle_arc(Vector2.ZERO, radius - thickness, 0.0, TAU, bg_color)
 
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	queue_redraw()
 
 
 func animate(duration: float, reverse: bool = false, initial_value: float = 0.0)\
 		-> void:
 	var tween: Tween = create_tween()
-	var from: int = max_value if reverse else initial_value
-	var to: int = initial_value if reverse else max_value
+	var from: float = max_value if reverse else initial_value
+	var to: float = initial_value if reverse else max_value
 	tween.tween_method(set_progress, from, to, duration)\
 			.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
 	await tween.finished
