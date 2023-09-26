f8f21970d0c55f4dca85fa37a6bed3d8e2dc6964
daveTheOldCoder <55119684+daveTheOldCoder@users.noreply.github.com>
2023-09-26 17:42:16 -0400

    Static typing and cleanup.

--- addons/radial_progress/RadialProgressBar.gd
+++ addons/radial_progress/RadialProgressBar.gd
@@ -2,59 +2,66 @@
 extends Control
 
 
-@export var max_value = 100;
-@export var radius = 120;
-@export var progress = 0;
-@export var thickness = 20 ;
-@export var bg_color : Color  = Color(0.5,.5,0.5,1);
-@export var bar_color :Color =  Color(0.2,.9,0.2,1);
-var angle = 0.0 ;
-func _ready():
-	pass # Replace with function body.
+@export var max_value: float = 100.0
+@export var radius: float = 120.0
+@export var progress: float = 0.0
+@export var thickness: float = 20.0
+@export var bg_color := Color(0.5, 0.5, 0.5, 1.0)
+@export var bar_color := Color(0.2, 0.9, 0.2, 1.0)
 
-func set_max(value):
-	max_value = value;
-	_draw();
 
-func set_progress(v):
-	progress = v;
-	queue_redraw();
+func set_max(value: float) -> void:
+	max_value = value
+	_draw()
 
-func set_radius_and_thickness(r,th):
-	radius = r; 
-	thickness = th;
-	queue_redraw();
 
-func set_colors(bg,bar):
-	bg_color = bg;
-	bar_color = bar;
-	queue_redraw();
+func set_progress(v: float) -> void:
+	progress = v
+	queue_redraw()
 
-func _draw():
-	draw_circle_arc(Vector2(0,0),radius,0,360,bg_color);
-	angle = progress*360/max_value;
-	draw_circle_arc(Vector2(0,0),radius,0,angle,bar_color);
-	draw_circle_arc(Vector2(0,0),radius - thickness,0,360,bg_color);
-	pass;
+
+func set_radius_and_thickness(r: float, th: float) -> void:
+	radius = r
+	thickness = th
+	queue_redraw()
+
+
+func set_colors(bg: Color, bar: Color) -> void:
+	bg_color = bg
+	bar_color = bar
+	queue_redraw()
+
+
+func _draw() -> void:
+	draw_circle_arc(Vector2.ZERO, radius, 0.0, TAU, bg_color)
+	var angle: float = progress * TAU / max_value
+	draw_circle_arc(Vector2.ZERO, radius, 0.0, angle, bar_color)
+	draw_circle_arc(Vector2.ZERO, radius - thickness, 0.0, TAU, bg_color)
+
 	
-func _process(delta):
-	queue_redraw();
+func _process(delta: float) -> void:
+	queue_redraw()
 
-func animate(duration,reverse = false,initial_value = 0):
+
+func animate(duration: float, reverse: bool = false, initial_value: float = 0.0)\
+		-> void:
 	var tween: Tween = create_tween()
-	if reverse:
-		tween.tween_method(set_progress, max_value, initial_value, duration)\
-				.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
-	else:
-		tween.tween_method(set_progress, initial_value, max_value, duration)\
-				.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+	var from: int = max_value if reverse else initial_value
+	var to: int = initial_value if reverse else max_value
+	tween.tween_method(set_progress, from, to, duration)\
+			.set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN)
+	await tween.finished
 
-func draw_circle_arc(center, radius, angle_from, angle_to, color):
-	var nb_points = 32
-	var points_arc = PackedVector2Array()
+
+func draw_circle_arc(center: Vector2, radius: float, angle_from: float,\
+		angle_to: float, color: Color) -> void:
+	var nb_points: int = 32
+	var points_arc := PackedVector2Array()
 	points_arc.push_back(center)
-	var colors = PackedColorArray([color])
-	for i in range(nb_points + 1):
-		var angle_point = deg_to_rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+	var colors := PackedColorArray([color])
+	var a: float = angle_from - (PI / 2.0)
+	var b: float = (angle_to - angle_from) / float(nb_points)
+	for i: int in range(nb_points + 1):
+		var angle_point: float = a + float(i) * b
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 	draw_polygon(points_arc, colors)
