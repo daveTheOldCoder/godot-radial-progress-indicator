829ef7172d003f9b9fc64c3f18f84ca7da8ef7b5
daveTheOldCoder <55119684+daveTheOldCoder@users.noreply.github.com>
2023-09-26 17:43:00 -0400

    Added reverse demo.

--- main.gd
+++ main.gd
@@ -3,5 +3,12 @@ extends Node2D
 @onready var button: Button = $RunDemo
 @onready var radial_progress: Control = $RadialProgress
 
+
 func _ready() -> void:
-	button.pressed.connect(func(): radial_progress.animate(3.0))
+	button.pressed.connect(_on_button_pressed)
+
+
+func _on_button_pressed() -> void:
+	await radial_progress.animate(2.0)
+	await get_tree().create_timer(1.0).timeout
+	await radial_progress.animate(2.0, true)
