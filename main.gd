2ec85d913481f372735ef8d8a4e4f3d73ab820f0
daveTheOldCoder <55119684+daveTheOldCoder@users.noreply.github.com>
2023-09-26 17:59:02 -0400

    Disable button while demo is running.

--- main.gd
+++ main.gd
@@ -9,6 +9,8 @@ func _ready() -> void:
 
 
 func _on_button_pressed() -> void:
+	button.disabled = true
 	await radial_progress.animate(2.0)
 	await get_tree().create_timer(1.0).timeout
 	await radial_progress.animate(2.0, true)
+	button.disabled = false
